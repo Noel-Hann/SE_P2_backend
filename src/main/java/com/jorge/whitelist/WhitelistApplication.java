7d604895698312a3ba4f2c606b915621847ef043
Jorge Moreno <jm.moreno3003@gmail.com>
2024-10-01 10:26:38 -0700

    adding heroku

--- src/main/java/com/jorge/whitelist/WhitelistApplication.java
+++ src/main/java/com/jorge/whitelist/WhitelistApplication.java
@@ -2,6 +2,8 @@ package com.jorge.whitelist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class WhitelistApplication {
@@ -9,5 +11,11 @@ public class WhitelistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WhitelistApplication.class, args);
 	}
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 }
---
+++ src/main/java/com/jorge/whitelist/models/User.java
@@ -0,0 +1,38 @@
+package com.jorge.whitelist.models;
+
+// import javax.persistence.Entity;
+// import javax.persistence.Id;
+
+// @Entity
+public class User {
+//     @Id
+//     private String userKey;
+//     private String username;
+//     private String password; // Ensure you handle passwords securely!
+
+//     // Getters and Setters
+//     public String getUserKey() {
+//         return userKey;
+//     }
+
+//     public void setUserKey(String userKey) {
+//         this.userKey = userKey;
+//     }
+
+//     public String getUsername() {
+//         return username;
+//     }
+
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
+
+//     public String getPassword() {
+//         return password;
+//     }
+
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+}
+
