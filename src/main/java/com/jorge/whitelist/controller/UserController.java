d7690b105283f5d78aea416f27112ff906aa3740
Noel Hann <nhann@csumb.edu>
2024-10-09 19:43:11 -0700

    Fixed some formatting, and changed method to reflect change in the db design to only allow one of any username

--- src/main/java/com/jorge/whitelist/controller/UserController.java
+++ src/main/java/com/jorge/whitelist/controller/UserController.java
@@ -3,21 +3,11 @@ package com.jorge.whitelist.controller;
 import com.jorge.whitelist.models.User;
 import com.jorge.whitelist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.jorge.whitelist.models.Wishlist;
-import com.jorge.whitelist.repository.WishlistRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
 
 
 @RestController
@@ -27,16 +17,14 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/add")
     public User addUser(@RequestBody Map<String,String> body){
 
         String username = body.get("username");
         String password = body.get("password");
 
-        //if the username already exists, we don't want to make a new user with the same username
-        if (getByUsername(username) != null) {
-            return null;
-        }
+
         User newUser = new User(username, password);
 
         userRepository.save(newUser);
@@ -45,22 +33,16 @@ public class UserController {
 
     }
 
-//    @GetMapping("/test")
-//    public String test(){
-//        return "test";
-//    }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/get-all")
     public List<User> getAll(){
         return userRepository.findAll();
     }
 
-    /*the plan:
-        add a method to get a user by username
-        in the add function, call that function to see if that username already exists
-        if it does, don't add the user
-   */
 
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/get/{username}")
     public User getByUsername(@PathVariable String username){
         List<User> response = userRepository.findByUsername(username);
